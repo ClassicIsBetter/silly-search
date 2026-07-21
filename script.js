ac5020f192b6899129bc8413863ff0ecc04e3000
ClassicIsBetter <dexterbooth3@proton.me>
2026-07-21 19:27:10 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 async function search() {
 
     let query = document.getElementById("searchBox").value;
-
     let results = document.getElementById("results");
 
     results.innerHTML = "Searching...";
 
-    let url = 
-    "https://api.duckduckgo.com/?q=" 
+    let url =
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="
     + encodeURIComponent(query)
-    + "&format=json";
+    + "&format=json&origin=*";
 
     try {
 
         let response = await fetch(url);
-
         let data = await response.json();
 
         results.innerHTML = "";
 
-        if(data.AbstractText) {
+        if (data.query.search.length === 0) {
+            results.innerHTML = "No results found.";
+            return;
+        }
+
+        data.query.search.forEach(item => {
 
             results.innerHTML += `
-                <h2>${data.Heading}</h2>
-                <p>${data.AbstractText}</p>
-                <a href="${data.AbstractURL}">
-                ${data.AbstractURL}
+                <h2>${item.title}</h2>
+                <p>${item.snippet}...</p>
+                <a href="https://en.wikipedia.org/wiki/${encodeURIComponent(item.title)}" target="_blank">
+                Read more
                 </a>
+                <hr>
             `;
 
-        } else {
-
-            results.innerHTML = "No results found.";
-
-        }
+        });
 
     } catch(error) {
 
+        console.log(error);
         results.innerHTML = "Error loading search.";
 
     }
