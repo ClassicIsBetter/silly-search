dd564483f1ea2f85384e4d7fb37a4d2c9a0f178e
ClassicIsBetter <dexterbooth3@proton.me>
2026-07-21 20:25:43 +0930

    Update script.js

--- script.js
+++ script.js
@@ -6,26 +6,15 @@ async function search() {
     results.innerHTML = "Searching...";
 
     let response = await fetch(
-        "silly-search.dexterbooth3.workers.dev/?q=" + encodeURIComponent(query)
+        "https://silly-search.dexterbooth3.workers.dev/?q=" + encodeURIComponent(query)
     );
 
     let data = await response.json();
 
     results.innerHTML = "";
 
-    if (data.AbstractText) {
-
-        results.innerHTML = `
-            <h2>${data.Heading}</h2>
-            <p>${data.AbstractText}</p>
-            <a href="${data.AbstractURL}" target="_blank">
-            Open result
-            </a>
-        `;
-
-    } else {
-
-        results.innerHTML = "No results found.";
-
-    }
+results.innerHTML = `
+    <h2>${data.Heading || "No title"}</h2>
+    <p>${data.AbstractText || "No description found."}</p>
+`;
 }
